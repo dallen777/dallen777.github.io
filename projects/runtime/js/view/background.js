9fefef8d0cf1375baf83cbd2fa39174f13c9dbc0
dallen777 <destinyallen177@gmail.com>
2019-12-04 01:22:19 +0000

    save all

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -1,10 +1,10 @@
 var background = function (window) {
     'use strict';
-    
+
     window.opspark = window.opspark || {};
     var draw = window.opspark.draw;
     var createjs = window.createjs;
-    
+
     /*
      * Create a background view for our game application
      */
@@ -16,18 +16,20 @@ var background = function (window) {
         if(!ground || typeof(ground.y) == 'undefined') {
             throw new Error("Invalid ground argument");
         }
-        
+
         // useful variables
         var canvasWidth = app.canvas.width;
         var canvasHeight = app.canvas.height;
         var groundY = ground.y;
-        
+
         // container which will be returned
         var background;
-        
+
         // ANIMATION VARIABLES HERE:
-        
-     
+        var buildings = [];
+        var tree;
+
+
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
         function render() {
@@ -38,7 +40,7 @@ var background = function (window) {
             // you should modify this to suit your game
             var backgroundFill = draw.rect(canvasWidth,canvasHeight,'black');
             background.addChild(backgroundFill);
-            
+
             // TODO: 3 - Add a moon and starfield
             var circle;
             for(var i=0;i<100;i++) {
@@ -47,19 +49,31 @@ var background = function (window) {
                 circle.y = groundY*Math.random();
                 background.addChild(circle);
             }
-        var moon = draw.bitmap('img/moon.png');
-        moon.x = 300;
-        moon.y = 25;
-        moon.scale
+            var moon = draw.bitmap('img/moon.png');
+            moon.x = 300;
+            moon.y = 25;
+            moon.scaleX = 1.0;
+            moon.scaleY = 1.0;
+            background.addChild(moon);
             // TODO: 5 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
-            
-            
+
+            for(var i=0;i<5;++i) {
+                var buildingHeight = 300;
+                var buildingHeight = draw.rect(75, buildingHeight, 'LightGray', 'Black', 1);\
+                buildingHeight.x = 200*i;
+                buildingHeight.y = groundY-buildingHeight;
+                background.addChild(building);
+                buildings.push(building);
+            }
+
             // TODO 4: Part 1 - Add a tree
-            
-            
+            tree = draw.bitmap('img/tree.png');
+            tree.x = 0;
+            tree.y = 0;
+            background.addChild(tree);
         } // end of render function - DO NOT DELETE
-        
-        
+
+
         // Perform background animation
         // called on each timer "tick" - 60 times per second
         function update() {
@@ -67,26 +81,29 @@ var background = function (window) {
             var canvasWidth = app.canvas.width;
             var canvasHeight = app.canvas.height;
             var groundY = ground.y;
-            
+
             // TODO 4: Part 2 - Move the tree!
-            
-            
+            tree.x = tree.x + 1;
+            if(tree.x < -200) {
+                tree.x = canvasWidth;
+            }
+
             // TODO 5: Part 2 - Parallax
-            
+
 
         } // end of update function - DO NOT DELETE
-        
-        
-        
+
+
+
         /* Make a createjs Container for the background and let it know about the render and upate functions*/
         background = new createjs.Container();
         background.resize = render;
         background.update = update;
-        
+
         /* make the background able to respond to resizing and timer updates*/
         app.addResizeable(background);
         app.addUpdateable(background);
-        
+
         /* render and return the background */
         render();
         return background;
